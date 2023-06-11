52e9bb527eb0242c6506ac151d5d51758d986d5c
Adithya Sailesh <97180744+Adithya-Sailesh@users.noreply.github.com>
2023-06-11 17:35:49 +0530

    Update RoundRobin.c

--- Scheduling/RoundRobin.c
+++ Scheduling/RoundRobin.c
@@ -8,24 +8,21 @@ struct rr{
     int wtime;
 }p[10],temp;
 void main(){
-    int n,i,ts,rt[10],time=0,flag=0,remain;
+    int n,i=0,ts,rt[10],time=0,flag=0,remain;
     float ttat=0,twt=0;
     printf("Enter the number of process");
     scanf("%d",&n);
     printf("Enter the time slice process");
     scanf("%d",&ts);
-    for(int i=0;i<n;i++){
-        p[i].pid=i;
-        printf("Enter the arival time of P%d",i);
-        scanf("%d",&p[i].atime);
-        printf("Enter the burst time of P%d",i);
-        scanf("%d",&p[i].btime);
-        rt[i]=p[i].btime;
+    for(int j=0;j<n;j++){
+        p[j].pid=j;
+        printf("Enter the arival time of P%d",j);
+        scanf("%d",&p[j].atime);
+        printf("Enter the burst time of P%d",j);
+        scanf("%d",&p[j].btime);
+        rt[j]=p[j].btime;
     }remain=n;
-    for(int i=0;i<n;i++){
-        printf("%d \t",rt[i]);
-    }
-    for(time=0,i=0;remain!=0;){
+    while(remain!=0){
         if(rt[i]<=ts && rt[i]>0){
             time=time+rt[i];
             rt[i]=0;
